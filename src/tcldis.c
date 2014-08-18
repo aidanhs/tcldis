de6c420bbacee6df53b299b25c3ceb260528f4d2
Aidan Hobson Sayers <aidanhs@cantab.net>
2014-08-18 02:58:00 +0100

    Create interp once

--- src/tcldis.c
+++ src/tcldis.c
@@ -1,6 +1,8 @@
 #include <Python.h>
 #include <tcl.h>
 
+static Tcl_Interp *interp;
+
 static PyObject *
 tcldis_test(PyObject *self, PyObject *args, PyObject *kwargs)
 {
@@ -9,16 +11,12 @@ tcldis_test(PyObject *self, PyObject *args, PyObject *kwargs)
 	if (!PyArg_ParseTupleAndKeywords(args, kwargs, "", kwlist))
 		return NULL;
 
-	Tcl_Interp *interp = Tcl_CreateInterp();
-
 	Tcl_Obj *tObj = Tcl_NewObj();
 	Tcl_AppendStringsToObj(tObj, "puts 1", NULL);
 
 	const Tcl_ObjType *bct = Tcl_GetObjType("bytecode");
 	if (Tcl_ConvertToType(interp, tObj, bct) != TCL_OK)
 		return PyLong_FromLong(1);
-
-	Tcl_DeleteInterp(interp);
 
 	return PyLong_FromLong(0);
 }
@@ -32,6 +30,8 @@ static PyMethodDef TclDisMethods[] = {
 PyMODINIT_FUNC
 init_tcldis(void)
 {
+	interp = Tcl_CreateInterp();
+
 	PyObject *m = Py_InitModule("_tcldis", TclDisMethods);
 	if (m == NULL)
 		return;
