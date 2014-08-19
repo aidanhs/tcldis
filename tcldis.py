38a9f75bc87dbfb698b852145b7d62e9913602b4
Aidan Hobson Sayers <aidanhs@cantab.net>
2014-08-19 10:13:14 +0100

    Stop trying to be overly clever

--- tcldis.py
+++ tcldis.py
@@ -1,12 +1,6 @@
 from __future__ import print_function
 
-def _tcldis_init():
-    import sys
-    import _tcldis
-    mod = sys.modules[__name__]
-    for key, value in _tcldis.__dict__.iteritems():
-        if not callable(value):
-            continue
-        mod.__dict__[key] = value
-
-_tcldis_init()
+import _tcldis
+printbc = _tcldis.printbc
+getbc = _tcldis.getbc
+inst_table = _tcldis.inst_table
