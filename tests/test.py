085d9955f09aec25ed8b999654f5138616c44506
Aidan Hobson Sayers <aidanhs@cantab.net>
2014-08-27 15:59:53 +0100

    Proc return works now

--- tests/test.py
+++ tests/test.py
@@ -103,7 +103,7 @@ def setupcase(test_class, name, case):
 
 for name, case in cases:
     setupcase(TestTclScript, name, case)
-    if name in ('set', 'set_array', 'ref', 'ref_array', 'incr', 'variable'):
+    if name in ('set', 'set_array', 'ref', 'ref_array', 'incr', 'variable', 'return'):
         setupcase(TestTclProc, name, case)
 
 if __name__ == '__main__':
